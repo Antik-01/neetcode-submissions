ba004a40f2025c94392eeea872cb31261a45488b
Antik <154757886+Antik-01@users.noreply.github.com>
2026-05-07 23:40:09 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length, m=1;
+        int[] pref = new int[n];
+        int[] suff = new int[n];
+        for(int i=0; i<n; i++){
+            pref[i] = m;
+            m *= nums[i];
+        }
+        m=1;
+        for(int i=n-1; i>=0; i--){
+            suff[i] = m;
+            m *= nums[i];
+        }
+        int[] res = new int[n];
+        for(int i=0; i<n; i++){
+            res[i] = pref[i]*suff[i];
+        }
+        return res;
+        // boolean zeroPresent=false;
+        // int cntZero = 0;
+        // for(int n: nums){
+        //     if(n==0){
+        //         zeroPresent = true;
+        //         cntZero++;
+        //     }
+        //     else antik*=n;
+        // }
+        // if(cntZero>1) return new int[nums.length];
+
+        // int[] arr = new int[nums.length];
+        // for(int i=0; i<arr.length; i++){
+        //     if(zeroPresent){
+        //         if(nums[i]!=0){
+        //             arr[i] = 0;
+        //         }
+        //         else arr[i] = antik;
+        //     }
+        //     else arr[i] = antik/nums[i];
+        // }
+        // return arr;
+    }
+}  
