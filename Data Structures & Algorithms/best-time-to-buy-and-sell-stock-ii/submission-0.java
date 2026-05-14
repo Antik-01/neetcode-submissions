d2c39606c70955335179cf99e5fd6e514896b8d7
Antik <154757886+Antik-01@users.noreply.github.com>
2026-05-14 21:05:38 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, low = prices[0];
+        for(int i=1; i< prices.length; i++){
+            if(prices[i]<low) low = prices[i];
+            else if(prices[i]>=low){
+                profit += (prices[i]-low);
+                low = prices[i];
+            }
+        }
+        return profit;
+    }
+}
