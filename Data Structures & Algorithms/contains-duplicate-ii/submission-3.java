beab4a7d8198490382cf87871e6d5afdabef8dda
Antik <154757886+Antik-01@users.noreply.github.com>
2026-05-17 21:22:18 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums.length==1 || k == 0) return false;
+        int l=0, r=1;
+        while(r<nums.length){
+            if(Math.abs(l-r) <= k){
+                if(nums[l] == nums[r]) return true;
+                r++;
+            }
+            else{
+                while(l<r-1){
+                    if(nums[++l] == nums[r]) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
